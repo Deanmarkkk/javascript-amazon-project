b10b9da02d6a888058e9bd8816b70a3277fbe8cb
Dean Mark <deansy0718@gmail.com>
2025-07-17 18:42:05 +0800

    generate the html of the checkout file using js

--- js/checkout.js
+++ js/checkout.js
@@ -1,7 +1,18 @@
 import { cart } from "../data/cart.js";
+import { products } from "../data/products.js";
 
 let storeHtml = '';
 cart.forEach((cartItem) => {
+    let productId = cartItem.productId;
+    
+    let matchingItem;
+    products.forEach((item) => {
+      if (item.id === productId) {
+        matchingItem = item
+      }
+    });
+    console.log(matchingItem);
+
     storeHtml += `
         <div class="cart-item-container">
             <div class="delivery-date">
@@ -10,18 +21,18 @@ cart.forEach((cartItem) => {
 
             <div class="cart-item-details-grid">
               <img class="product-image"
-                src="images/products/athletic-cotton-socks-6-pairs.jpg">
+                src="${matchingItem.image}">
 
               <div class="cart-item-details">
                 <div class="product-name">
-                  Black and Gray Athletic Cotton Socks - 6 Pairs
+                  ${matchingItem.name}
                 </div>
                 <div class="product-price">
-                  $10.90
+                  $${(matchingItem.priceCents / 100).toFixed(2)}
                 </div>
                 <div class="product-quantity">
                   <span>
-                    Quantity: <span class="quantity-label">2</span>
+                    Quantity: <span class="quantity-label">${cartItem.quantity}</span>
                   </span>
                   <span class="update-quantity-link link-primary">
                     Update
